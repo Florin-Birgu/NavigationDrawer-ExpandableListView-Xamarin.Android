c136f9920f65a48fe607e4a84675e42409eb6b89
Florin Birgu <florin@disruption.ro>
2017-01-07 13:07:30 +0200

    Add project files.

---
+++ NavigationDrawer ExpandableListView/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace NavigationDrawer_ExpandableListView
+{
+    [Activity(Label = "NavigationDrawer_ExpandableListView", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
